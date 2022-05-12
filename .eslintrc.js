df651111f0cfee282247afb5ce55385160b25714
Emanuele De Cupis <ema.decu@gmail.com>
2022-05-12 11:39:05 +0200

    project setup

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  ignorePatterns: [
+    "node_modules",
+    "generated",
+    "**/__tests__/*",
+    "**/__mocks__/*",
+    "**/__integrations__/*",
+    "**/templates/html/**",
+    "Dangerfile.*",
+    "*.d.ts"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    sourceType: "module"
+  },
+  extends: ["@pagopa/eslint-config/strong"],
+  rules: {}
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+Code Sample supporting our workshop at [Codemotion Workshop Fest 2022](https://events.codemotion.com/conferences/online/2022/workshop-fest).
